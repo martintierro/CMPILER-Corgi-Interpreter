541c6d2b267d6057aca728ebb25f53521c491a41
Martin Tierro <martintierro15@gmail.com>
2021-02-02 23:56:31 +0800

    Fixed recognition of keywords. Note: evaluation with && is not working properly

--- src/Utlities/KeywordRecognizer.java
+++ src/Utlities/KeywordRecognizer.java
@@ -1,11 +1,11 @@
 package Utlities;
 
 public class KeywordRecognizer {
-    public static String PRIMITIVE_TYPE_BOOLEAN = "boolean";
+    public static String PRIMITIVE_TYPE_BOOLEAN = "bool";
     public static String PRIMITIVE_TYPE_CHAR = "char";
     public static String PRIMITIVE_TYPE_INT = "int";
     public static String PRIMITIVE_TYPE_FLOAT = "float";
-    public static String PRIMITIVE_TYPE_STRING = "String";
+    public static String PRIMITIVE_TYPE_STRING = "string";
 
     public static String BOOLEAN_TRUE = "true";
     public static String BOOLEAN_FALSE = "false";
