8461496a0b78e7c0f017907c9bc782a87355df4c
Martin Tierro <martintierro15@gmail.com>
2021-02-03 19:17:08 +0800

    Fixed semantic error printing

--- src/Builder/BuildChecker.java
+++ src/Builder/BuildChecker.java
@@ -1,15 +1,20 @@
 package Builder;
 
-import org.antlr.v4.runtime.ANTLRErrorListener;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import ANTLR.CorgiError;
+import GeneratedAntlrClasses.CorgiLexer;
+import IDE.View;
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.util.BitSet;
+import java.util.Collections;
+import java.util.List;
 
-public class BuildChecker implements ANTLRErrorListener {
+public class BuildChecker extends BaseErrorListener {
+
+    private final String[] keywords = new String[]{"const", "void", "public", "private", "static", "final", "int", "double", "short", "long", "char", "float", "woof"};
 
     private static BuildChecker sharedInstance = null;
 
@@ -77,7 +82,21 @@ public class BuildChecker implements ANTLRErrorListener {
 
     public static void reportCustomError(int errorCode, String additionalMessage, Object... parameters) {
         String errorMessage = String.format(SemanticErrorDictionary.getErrorMessage(errorCode) + " " + additionalMessage, parameters);
-        System.err.println(errorMessage); //TODO Change to IDE
+        System.out.println("ERROR: " + errorMessage);
+        String[] s = errorMessage.split("line [0-9]+");
+        String line = errorMessage.substring(errorMessage.indexOf("line"), errorMessage.indexOf("."));
+
+        String number = errorMessage.substring(errorMessage.indexOf("line ") + 5, errorMessage.indexOf(s[1]));
+
+        CorgiError corgiError = new CorgiError();
+
+        corgiError.setLineNumber( Integer.parseInt(number) );
+        corgiError.setErrorPrefix(s[0]);
+        corgiError.setLineLayout(line);
+        corgiError.setErrorSuffix(s[1]);
+
+        View.appendErrorInConsole(corgiError);
+
 
         sharedInstance.successful = false;
     }
